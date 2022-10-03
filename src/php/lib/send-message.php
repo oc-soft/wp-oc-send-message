43d36b127c7d58199a2b2deec4abf30467868c91
Toshiyuki Ogawa <toshi@oc-soft.net>
2022-10-04 07:32:12 +0900

    I released this plugin at a site.

--- src/php/lib/send-message.php
+++ src/php/lib/send-message.php
@@ -22,8 +22,15 @@ class SendMessage {
             $headers[] = "from:$from";
         }
 
-        error_log(ini_get('sendmail_path'));
+        add_filter('wp_mail_from', function($from) {
+            error_log("oc-send-message from: $from");
+            return $from;
+        });
 
+        add_filter('wp_mail_content_type', function($content_type) {
+            error_log("oc-send-message content type: $content_type");
+            return $content_type;
+        });
         wp_mail($to, $subject, $body, $headers);
         
     }
